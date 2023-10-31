fe33b360811d75a742906758e453e551a80c481a
tomasamori <tomas.amori@gmail.com>
2023-10-31 17:11:37 -0300

    fix file extension

--- practicas/p5-php/ej4-contador.php
+++ practicas/p5-php/ej4-contador.php
@@ -3,12 +3,12 @@
 ?>
 <html>
     <body>
-        <a href="ej4-index.html"></a>
+        <a href="ej4-index.php"></a>
         <?php
             echo "Usted ha visitado " . ($_SESSION["contador"]) . " páginas";
         ?>
         <br>
         <br>
-        <a href="ej4-index.html">Volver a la página principal</a>
+        <a href="ej4-index.php">Volver a la página principal</a>
     </body>
 </html>
